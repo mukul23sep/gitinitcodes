f248afd8087676e924faa606c36d0e1ed183bda5
mukul23sep <mukulanand01k@gmail.com>
2021-12-20 14:43:54 +0530

    sum

--- rough.c
+++ rough.c
@@ -1,4 +1,9 @@
-#include<stdio.h>
-int main(){
-    printf("hello world");
+#include <stdio.h>
+int main() {    
+
+    int a, b, 
+    // printf("Enter two number");
+    sum = a + b;      
+     printf("%d + %d = %d", a, b, sum);
+    return 0;
 }
