0767b96c3a33bb17f00903119a0c45474e330502
mukul23sep <mukulanand01k@gmail.com>
2022-03-16 11:22:07 +0530

    loop

---
+++ loops.java
@@ -0,0 +1,12 @@
+package Mukul.gitinitcodes;
+
+public class loops {
+
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i = i + 1;
+        } while (1 < 110);
+    }
+}
