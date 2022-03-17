9326fe957bce6956492f28a896bf1d3e805c2f51
mukul23sep <mukulanand01k@gmail.com>
2022-03-17 12:28:54 +0530

    update bit

--- Bits.java
+++ Bits.java
@@ -2,11 +2,23 @@ import java.util.*;
 
 public class Bits {
     public static void main(String args[]) {
-        int n = 5; // 0101
-        int pos = 2;
+        Scanner sc = new Scanner(System.in);
+        int oper = sc.nextInt();
+        // oper=1 -> set; oper=0 -> clear
+        int n = 5;
+        int pos = 1;
+
         int bitMask = 1 << pos;
-        int newBitMask = ~(bitMask);
-        int newNumber = newBitMask & n;
-        System.out.println(newNumber);
+        if (oper == 1) {
+            // set
+            int newNumber = bitMask | n;
+            System.out.println(newNumber);
+        } else {
+            // clear
+            int newBitMask = ~(bitMask);
+            int newNumber = newBitMask & n;
+            System.out.println(newNumber);
+        }
+
     }
 }
