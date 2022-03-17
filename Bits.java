13d78401feef3811a71f8658991930b5a60a7685
mukul23sep <mukulanand01k@gmail.com>
2022-03-17 12:19:48 +0530

    clear bit

--- Bits.java
+++ Bits.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class Bits {
     public static void main(String args[]) {
         int n = 5; // 0101
-        int pos = 1;
+        int pos = 2;
         int bitMask = 1 << pos;
-
-        int newNumber = bitMask | n;
+        int newBitMask = ~(bitMask);
+        int newNumber = newBitMask & n;
         System.out.println(newNumber);
     }
 }
