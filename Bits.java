a260f66ccb023c3a8bc96743973c8c82559d7561
mukul23sep <mukulanand01k@gmail.com>
2022-03-17 12:16:54 +0530

    set bit

--- Bits.java
+++ Bits.java
@@ -1,17 +1,12 @@
-package gitinitcodes;
-
 import java.util.*;
 
 public class Bits {
-    public static void main(String[] args) {
-        int n = 5;
+    public static void main(String args[]) {
+        int n = 5; // 0101
         int pos = 1;
         int bitMask = 1 << pos;
-        if ((bitMask & n) == 0) {
-            System.out.println("bit was zero");
-        } else {
-            System.out.println("bit was one");
-        }
-    }
 
+        int newNumber = bitMask | n;
+        System.out.println(newNumber);
+    }
 }
