7734c3c39763d488644e9abbef9c6ea0725a50c9
mukul23sep <mukulanand01k@gmail.com>
2022-08-18 18:51:37 +0530

    add number

---
+++ addnumber.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cin >> n;
+    int sum = 0;
+    while (n > 0)
+    {
+        int = sum + last_digit;
+
+        n = n / 10;
+    }
+    return 0;
+}
